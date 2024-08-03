62382a11bee05adb652f134718b6ed55c31c0123
NomaTyx <jjziebart99@gmail.com>
2024-08-03 13:13:23 -0700

    changed questions to make them easier to test with

--- lib/question_list.dart
+++ lib/question_list.dart
@@ -10,31 +10,44 @@ class Question {
 const List sample_data = [
   {
     "id": 1,
-    "question":
-    "EASY: Flutter is an open-source UI software development kit created by ______",
-    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "question": "EASY: PLACEHOLDER EASY QUESTION #1",
+    "options": ['wrong', 'right', 'wrong', 'wrong'],
     "answer_index": 1,
     "difficulty": 1,
   },
   {
     "id": 2,
-    "question": "MEDIUM: When did Google release Flutter",
-    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "question": "MEDIUM: PLACEHOLDER MEDIUM QUESTION #1",
+    "options": ['wrong', 'wrong', 'right', 'wrong'],
     "answer_index": 2,
     "difficulty": 2,
   },
   {
     "id": 3,
-    "question": "HARD: A memory location that holds a single letter or number.",
-    "options": ['Double', 'Int', 'Char', 'Word'],
+    "question": "HARD: PLACEHOLDER HARD QUESTION #1",
+    "options": ['wrong', 'wrong', 'right', 'wrong'],
     "answer_index": 2,
     "difficulty": 3,
   },
   {
     "id": 4,
-    "question": "EASY: What command do you use to output data to the screen?",
-    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "question": "MEDIUM: PLACEHOLDER MEDIUM QUESTION #2",
+    "options": ['wrong', 'wrong', 'right', 'wrong'],
     "answer_index": 2,
     "difficulty": 2,
+  },
+  {
+    "id": 5,
+    "question": "EASY: PLACEHOLDER EASY QUESTION #2",
+    "options": ['wrong', 'right', 'wrong', 'wrong'],
+    "answer_index": 1,
+    "difficulty": 1,
+  },
+  {
+    "id": 6,
+    "question": "HARD: PLACEHOLDER HARD QUESTION #2",
+    "options": ['right', 'wrong', 'wrong', 'wrong'],
+    "answer_index": 0,
+    "difficulty": 3,
   },
 ];
