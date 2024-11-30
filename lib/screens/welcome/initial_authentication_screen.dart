196a1214fdf27952b4f13661b9969e9cba66c03e
NomaTyx <jjziebart99@gmail.com>
2024-11-29 17:14:23 -0800

    finished initial auth screen which checks if the device is registered on the database, and asks the user for their name and region if not.

--- lib/screens/welcome/initial_authentication_screen.dart
+++ lib/screens/welcome/initial_authentication_screen.dart
@@ -1,37 +1,45 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geography_trivia_app/screens/signup_screen.dart';
+import 'package:geography_trivia_app/screens/welcome/home_screen.dart';
 import 'package:get/get.dart';
 import 'package:geography_trivia_app/controllers/auth_services.dart';
 
-const List<String> regionList = <String>['Africa', 'Asia', 'Europe', 'North America', 'Oceania', 'South America', 'Antarctica'];
+import '../../controllers/player_data_controller.dart';
 
 class NameScreen extends StatelessWidget {
   const NameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    PlayerDataController playerDataController = Get.put(PlayerDataController()) ?? Get.find<PlayerDataController>();
     String name = '';
+
     return Scaffold(
       backgroundColor: Colors.grey[800],
+
       body: Padding(
         padding: const EdgeInsets.fromLTRB(30.0, 90.0, 30.0, 0),
+
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+
             const Center(
               child: CircleAvatar(
                 backgroundImage: AssetImage('assets/toge.jpg'),
                 radius: 80.0,
               ),
             ),
+
             Divider(
               height: 30,
               color: Colors.grey[800],
             ),
+
             const Center(
-              child: Text(
-                'Hi! Please enter your name.',
+              child:
+              Text('Hi! Please enter your name.',
                 style: TextStyle(
                   letterSpacing: 2.0,
                   fontSize: 30.0,
@@ -40,24 +48,28 @@ class NameScreen extends StatelessWidget {
                 ),
               ),
             ),
+
             Divider(
               height: 30,
               color: Colors.grey[800],
             ),
+
             TextField(
               decoration: InputDecoration(hintText: "Name"),
               onChanged: (String value) {
                 name = value;
               },
             ),
+
             Divider(
               height: 30,
               color: Colors.grey[800],
             ),
+
             ElevatedButton(
                 onPressed: () => {
                   if(name != '') {
-                    //TODO: add a player data controller and have it put the player's name in there.
+                    playerDataController.setPlayerName(name),
                     Get.to(RegionScreen())
                   }
                   else {
@@ -76,6 +88,7 @@ class NameScreen extends StatelessWidget {
                     ),
                   }
                 },
+
                 child: Text("Done"))
           ],
         ),
@@ -85,7 +98,9 @@ class NameScreen extends StatelessWidget {
 }
 
 class RegionScreen extends StatelessWidget {
-  const RegionScreen({super.key});
+  RegionScreen({super.key});
+  final List<String> regionList = <String>['Africa', 'Asia', 'Europe', 'North America', 'Oceania', 'South America', 'Antarctica'];
+  PlayerDataController playerDataController = Get.put(PlayerDataController()) ?? Get.find<PlayerDataController>();
 
   @override
   Widget build(BuildContext context) {
@@ -100,7 +115,7 @@ class RegionScreen extends StatelessWidget {
             const Center(
               child: CircleAvatar(
                 backgroundImage: AssetImage('assets/toge.jpg'),
-                radius: 80.0,
+                radius: 40.0,
               ),
             ),
             Divider(
@@ -122,16 +137,42 @@ class RegionScreen extends StatelessWidget {
               height: 30,
               color: Colors.grey[800],
             ),
-            //TODO this needs to be a dropdown
-            Divider(
-              height: 30,
-              color: Colors.grey[800],
-            ),
-            ElevatedButton(
-                onPressed: () => {
-
+            ListView.separated(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(9),
+                itemCount: regionList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 50,
+                    child: ElevatedButton(
+                      child: Text('${regionList[index]}'),
+                      onPressed: () {
+                        if(regionList[index] != "Antarctica") {
+                          playerDataController.setPlayerRegion(regionList[index]);
+                          playerDataController.addUser();
+                          Get.to(HomeScreen());
+                        }
+                        else {
+                          showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text('Nice try, bucko'),
+                              content: const Text('You don\'t live in antarctica'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context, 'OK'),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                  );
                 },
-                child: Text("Done"))
+                separatorBuilder: (BuildContext context, int index) => const Divider(),
+            )
           ],
         ),
       ),
