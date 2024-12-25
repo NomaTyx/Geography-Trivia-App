1418d172536c9642723169c64012247268f4e3de
NomaTyx <jjziebart99@gmail.com>
2024-12-25 10:56:04 -0800

    made leaderboard display the data. data updates when you launch the app

--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:geography_trivia_app/controllers/player_data_controller.dart';
 import 'package:geography_trivia_app/screens/welcome/home_screen.dart';
@@ -38,17 +40,51 @@ class LeaderboardScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Container(
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Text("Rank", textAlign: TextAlign.center ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Text("Name", textAlign: TextAlign.center),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Text("Score", textAlign: TextAlign.center)
+                  ),
+                  Expanded(flex: 2, child: Text("Region", textAlign: TextAlign.center)),
+                ]
+            ),
+            SizedBox(
               height: 500,
               child:ListView.separated(
                 shrinkWrap: true,
-                padding: const EdgeInsets.all(9),
-                itemCount: 10,
+                padding: const EdgeInsets.all(15),
+                itemCount: min(playerDataController.topPlayersList.length, 10),
                 itemBuilder: (BuildContext context, int index) {
-                  return Container(
+                  return SizedBox(
                     height: 30,
-                    // row with rank, name, region, total score
-                    child: Text("placeholder " + (index + 1).toString()),
+                      // row with rank, name, region, total score
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Text("#${(index + 1).toString()}", textAlign: TextAlign.center),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Text(playerDataController.topPlayersList[index]["Name"], textAlign: TextAlign.center),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Text(playerDataController.topPlayersList[index]["Score"].toString(), textAlign: TextAlign.center)
+                        ),
+                        Expanded(flex: 2, child: Text(playerDataController.topPlayersList[index]["Region"], textAlign: TextAlign.center)),
+                      ]
+                    )
                   );
                 },
                 separatorBuilder: (BuildContext context, int index) => const Divider(),
@@ -61,8 +97,8 @@ class LeaderboardScreen extends StatelessWidget {
                 child: const Text('BACK TO HOME SCREEN')
             ),
             ElevatedButton(
-                onPressed: () {
-                  //playerDataController.GetTopPlayers(10);
+                onPressed: () async {
+                  await playerDataController.FindTopPlayers(10);
                 },
                 child: const Text('test top 10 list')
             )
